0d17a075209a37649253eccba9399b31ca6cfbde
Bianca Rosa <me@biancarosa.com.br>
2017-09-27 01:02:25 -0300

    Adiciona instrucoes

--- intro.m
+++ intro.m
@@ -1,60 +1,90 @@
 # Computação Gráfica (IME 04 - 10842)
+# Bianca Rosa de Mendonça
 # Introdução ao Octave/Matlab
-RED = [ 0 1 0; 1 1 0; 0 0 1];
-GREEN = [ 0 0.2 0.3; 0.5 0.75 0.2; 1 0 0];
-BLUE = [ 0 0 1; 0.2 0.3 0.7; 1 0 0.25];
+# Imagens
 
-RED;
-GREEN;
-BLUE;
+RED = [ 0 1 0; 1 1 0; 0 0 1]
+GREEN = [ 0 0.2 0.3; 0.5 0.75 0.2; 1 0 0]
+BLUE = [ 0 0 1; 0.2 0.3 0.7; 1 0 0.25]
 
-RGB = cat (3, RED, GREEN, BLUE);
+RED
+GREEN
+BLUE
 
-RGB;
+RGB = cat (3, RED, GREEN, BLUE)
 
-RGB(:,:,1);
-RGB(:,:,2);
-RGB(:,:,3);
+RGB
 
-imshow (RGB);
+RGB(:,:,1)
+RGB(:,:,2)
+RGB(:,:,3)
 
-MAP = [0.2 0.3 0.75; 0.21 0.57 0.54; 0.54 1 0];
-X = [0 1 2; 3 2 1];
+imshow (RGB)
+title("RGB 1")
 
-imshow(X, MAP);
+
+MAP = [0.2 0.3 0.75; 0.21 0.57 0.54; 0.54 1 0]
+X = [0 1 2; 3 2 1]
+
+imshow(X, MAP)
+title("Color map")
+
 
 # Tons de cinza
-X = [0 1 0.33; 0.75 0 1; 0 1 0.25];
-imshow(X);
-imshow (X, [0 1]);
-imshow (X, [0 2]);
-imshow (X, [0 16]);
+X = [0 1 0.33; 0.75 0 1; 0 1 0.25]
+imshow(X)
+title("Cinza 1")
+
+imshow (X, [0 1])
+title("Cinza 2")
+
+imshow (X, [0 2])
+title("Cinza 3")
+
+imshow (X, [0 16])
+title("Cinza 4")
+
 
 # Imagem binaria
-X = [0 1 0; 0 0 1; 0 1 0];
-imshow(X);
+X = [0 1 0; 0 0 1; 0 1 0]
+imshow(X)
+title("Imagem binaria")
 
 # Leitura de imagens
 arara_full = imread("imagens/arara_full.png");
 arara_full_256 = imread("imagens/arara_full_256.bmp");
-whos("arara*");
-imshow(arara_full);
-imshow(arara_full_256);
+whos("arara*")
+imshow(arara_full)
+imshow(arara_full_256)
 
 # Escrita de imagens
-imwrite(arara_full, "imagens/arara_full.bmp");
-imwrite(arara_full_256, "imagens/arara_full_256.png");
+
+
 arara_full_bmp = imread("imagens/arara_full.bmp");
 arara_full_256_png = imread("imagens/arara_full_256.png");
-imshow(arara_full);
-imshow(arara_full_256);
+imshow(arara_full)
+imshow(arara_full_256)
 
 # Conversao de imagens
-[X, MAP] = rgb2ind(arara_full);
-imshow(X,MAP);
+[X, MAP] = rgb2ind(arara_full)
+imshow(X,MAP)
+title("Img convertida")
 
 # Criando figures
 figure
 imshow(X,MAP)
+title("Araras")
+
 figure
 imshow(RGB)
+title("RGB")
+
+
+# Subplots
+subplot(2, 1, 1)
+imshow(X,MAP)
+title("Araras")
+subplot (2, 1, 2)
+imshow(RGB)
+title("RGB")
+
---
+++ matrizes.m
@@ -0,0 +1,35 @@
+# Computação Gráfica (IME 04 - 10842)
+# Bianca Rosa de Mendonça
+# Introdução ao Octave/Matlab
+# Matrizes
+
+A = [16 3 2 13 19; 5 10 11 8 3; 6 7 9 12 8; 4 15 14 1 13; 1 2 3 4 5]
+
+A(1,1) + A(1,4) + A(4,1) + A(4,4)
+
+A
+A(1,3) + A(3,1)
+A(1: 3,3)
+A(1:4,2:4)
+A(:, 3)
+A(1:3, :)
+
+B = [16 3 2 13 19; 10 11 8 3 6; 9 12 8 4 15;]
+A(:, [3 5 2]) = B(:, 1:3)
+
+zeros(5)
+
+Z = zeros(5,5)
+Z
+
+ones(5)
+
+O = ones(5,3)
+O
+
+A * Z
+A .* Z
+B * 2
+B / 2
+B * (O * 2)
+A / 3
