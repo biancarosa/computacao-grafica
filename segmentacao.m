85f1dfa6dee10c3a4eeb5f4bbf255243c3e91ad1
Bianca Rosa <me@biancarosa.com.br>
2018-01-31 20:28:13 -0200

    organiza codigo

--- segmentacao.m
+++ segmentacao.m
@@ -11,8 +11,6 @@ img = rgb2gray(img);
 img = im2double(img);
 
 sobel_horizontal = [-1 -2 -1; 0 0 0; 1 2 1];
-sobel_vertical = [-1 0 1; -2 0 2; -1 0 1];
-
 img_sh = imfilter(img, sobel_horizontal);
 figure;
 title("Sobel")
@@ -20,10 +18,12 @@ subplot(2, 1, 1);
 imshow(img_sh);
 title("Sobel Horizontal");
 
+sobel_vertical = [-1 0 1; -2 0 2; -1 0 1];
 img_sv = imfilter(img, sobel_vertical);
 subplot(2, 1, 2);
 imshow(img_sv);
 title("Sobel Vertical");
+print -djpg geradas/segmentacao1.jpg
 
 figure;
 sobel_diag1 = [ 0 1 2; -1 0 1; -2 -1 0];
@@ -37,75 +37,70 @@ img_sa2 = imfilter(img, sobel_diag2);
 subplot(2, 1, 2);
 imshow(img_sa2);
 title("Sobel Diagonal 2");
+print -djpg geradas/segmentacao2.jpg
 
 figure;
 subplot(2, 1, 1);
-filtered = img_sh + img_sv;
-imshow(filtered);
+shv = img_sh + img_sv;
+imshow(shv);
+title("Sobel Horizontal + Vertical")
 
-filtered = filtered - 0.25;
+filtered = shv - 0.25;
 subplot(2, 1, 2);
 imshow(filtered);
+title("Sobel Horizontal + Vertical")
+print -djpg geradas/segmentacao3.jpg
 
 figure;
 title("Sobel - funcao edge");
 subplot(1, 1, 1);
 edg = edge(img, "Sobel");
 imshow(edg);
+print -djpg geradas/segmentacao4.jpg
 
 figure;
 title("Canny - funcao edge");
 edg = edge(img, "Canny");
 imshow(edg);
+print -djpg geradas/segmentacao5.jpg
 
 figure;
 title("Kirsch - funcao edge");
 edg = edge(img, "Kirsch");
 imshow(edg);
+print -djpg geradas/segmentacao6.jpg
 
 figure;
 title("Lindeberg - funcao edge");
 edg = edge(img, "Lindeberg");
 imshow(edg);
+print -djpg geradas/segmentacao7.jpg
 
 figure;
 title("LoG - funcao edge");
 edg = edge(img, "LoG");
 imshow(edg);
+print -djpg geradas/segmentacao8.jpg
 
 figure;
 title("Roberts - funcao edge");
 edg = edge(img, "Roberts");
 imshow(edg);
+print -djpg geradas/segmentacao9.jpg
 
 figure;
 title("zerocross - funcao edge");
 edg = edge(img, "zerocross");
 imshow(edg);
+print -djpg geradas/segmentacao10.jpg
 
 figure;
 title("Andy - funcao edge");
 edg = edge(img, "Andy");
 imshow(edg);
+print -djpg geradas/segmentacao11.jpg
 
-nir = imread("imagens/laranjeiras_nir.tif");
-red = imread("imagens/laranjeiras_r.tif");
-blue = imread("imagens/laranjeiras_b.tif");
-green = imread("imagens/laranjeiras_g.tif");
-nir = im2double(nir);
-red = im2double(red);
-NDVI = (nir - red) / (nir + red);
-
-bw =  im2bw(NDVI, 0.09);
-
-digital = imread("imagens/digital.png");
-imshow(digital);
-digital = im2double(digital);
-imhist(digital);
-d = im2double(digital < 0.8);
-d = im2double(digital > 0.3);
-imshow(d);
-
+# TODO: Pintar bordas com angulo
 # Amarelo: 
 # π/6 > ângulo >  -π/6 
 # 30 > angulo > -30
@@ -122,3 +117,24 @@ imshow(d);
 # Azul:
 # -π/3 > ângulo > π/3
 # -60 > angulo > 60
+
+# NDVI
+nir = imread("imagens/laranjeiras_nir.tif");
+red = imread("imagens/laranjeiras_r.tif");
+blue = imread("imagens/laranjeiras_b.tif");
+green = imread("imagens/laranjeiras_g.tif");
+nir = im2double(nir);
+red = im2double(red);
+NDVI = (nir - red) / (nir + red);
+# TODO: Why this is all black and white
+bw =  im2bw(NDVI, 0.09);
+
+# Separação de imagens
+digital = imread("imagens/digital.png");
+imshow(digital);
+digital = im2double(digital);
+imhist(digital);
+d = im2double(digital < 0.8);
+d = im2double(digital > 0.3);
+imshow(d);
+print -djpg geradas/segmentacao12.jpg
