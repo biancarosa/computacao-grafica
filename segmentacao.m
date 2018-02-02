b614dc422bcfeed31cfeddfb7578f6780bd31ca5
Bianca Rosa <me@biancarosa.com.br>
2018-02-01 22:11:26 -0200

    finish stuff

--- segmentacao.m
+++ segmentacao.m
@@ -48,102 +48,97 @@ title("Sobel Horizontal + Vertical")
 filtered = shv - 0.25;
 subplot(2, 1, 2);
 imshow(filtered);
-title("Sobel Horizontal + Vertical")
+title("Sobel Horizontal + Vertical [Limiarizado]")
 print -djpg geradas/segmentacao3.jpg
 
-
+figure;
+subplot(1, 1, 1);
 magnitude = abs(img_sh) + abs(img_sv);
-angulo = atan(img_sv./img_sh);
+angulo = atan(img_sh ./ img_sv);
 imshow(angulo);
+title("Angulos")
 print -djpg geradas/segmentacao5.jpg
 
 # Pintar bordas de acordo c/ angulo
 angulo_ind = angulo;
 
-# Amarelo: 
-# π/6 > ângulo >  -π/6 
-# 30 > angulo > -30
-angulo_ind(find(angulo > -pi/6)) = 1;
+angulo_ind(find(angulo < pi/3)) = 2; # verde angulo < 60
+angulo_ind(find(angulo < pi/6)) = 1; # amarelo angulo < 30
+angulo_ind(find(angulo < -pi/3)) = 4; # azul angulo < -60
+angulo_ind(find(angulo < -pi/6)) = 3; # vermelho angulo < -30
 
-# Vermelho: 
-# -π/6 > ângulo > -π/3
-# -30 > angulo > -60
-angulo_ind(find(angulo > -pi/3)) = 3;
+angulo_ind(find(angulo > -pi/6)) = 1; # amarelo angulo > -30 
+angulo_ind(find(angulo > -pi/3)) = 3; # vermelho angulo > -60
+angulo_ind(find(angulo > pi/6)) = 2; #verde angulo > 30
+angulo_ind(find(angulo > pi/3)) = 4; # azul angulo > 60
 
-
-# Verde: 
-# π/3 > ângulo > π/6
-# 60 > angulo > 30
-angulo_ind(find(angulo > pi/6)) = 2;
-
-# Azul:
-# -π/3 > ângulo > π/3
-# -60 > angulo > 60
-angulo_ind(find(angulo > pi/3)) = 4;
-
-MAP = [0 0 0; 1 1 0; 0 1 0; 1 0 0;  0 0 1];
+MAP = [1 1 0; 0 1 0; 1 0 0;  0 0 1];
 figure;
 subplot(1, 1, 1);
 imshow(angulo_ind, MAP);
+title("Angulos Coloridos")
 print -djpg geradas/segmentacao6.jpg
 
 shv = img_sh + img_sv;
-shv(find(shv<0.2)) = 0;
-shv(find(shv>0.2)) = 1;
+shv(find(shv<=0.15)) = 0;
+shv(find(shv>0.15)) = 1;
 figure;
 imshow(shv);
+title("Img binaria - sobel")
 print -djpg geradas/segmentacao6_1.jpg
 
+MAP = [0 0 0; 1 1 0; 0 1 0; 1 0 0; 0 0 1];
 imshow(angulo_ind .* shv, MAP);
+title("Img colorida")
 print -djpg geradas/segmentacao7.jpg
 
 figure;
-title("Sobel - funcao edge");
 subplot(1, 1, 1);
 edg = edge(img, "Sobel");
 imshow(edg);
+title("Sobel - funcao edge");
 print -djpg geradas/segmentacao8.jpg
 
 figure;
-title("Canny - funcao edge");
 edg = edge(img, "Canny");
 imshow(edg);
+title("Canny - funcao edge");
 print -djpg geradas/segmentacao9.jpg
 
 figure;
-title("Kirsch - funcao edge");
 edg = edge(img, "Kirsch");
 imshow(edg);
+title("Kirsch - funcao edge");
 print -djpg geradas/segmentacao10.jpg
 
 figure;
-title("Lindeberg - funcao edge");
 edg = edge(img, "Lindeberg");
 imshow(edg);
+title("Lindeberg - funcao edge");
 print -djpg geradas/segmentacao11.jpg
 
 figure;
-title("LoG - funcao edge");
 edg = edge(img, "LoG");
 imshow(edg);
+title("LoG - funcao edge");
 print -djpg geradas/segmentacao12.jpg
 
 figure;
-title("Roberts - funcao edge");
 edg = edge(img, "Roberts");
 imshow(edg);
+title("Roberts - funcao edge");
 print -djpg geradas/segmentacao13.jpg
 
 figure;
-title("zerocross - funcao edge");
 edg = edge(img, "zerocross", 5, 15);
 imshow(edg);
+title("zerocross - funcao edge");
 print -djpg geradas/segmentacao14.jpg
 
 figure;
-title("Andy - funcao edge");
 edg = edge(img, "Andy");
 imshow(edg);
+title("Andy - funcao edge");
 print -djpg geradas/segmentacao15.jpg
 
 
@@ -154,16 +149,38 @@ blue = imread("imagens/laranjeiras_b.tif");
 green = imread("imagens/laranjeiras_g.tif");
 nir = im2double(nir);
 red = im2double(red);
-NDVI = (nir - red) / (nir + red);
-# TODO: Why this is all black and white
-bw =  im2bw(NDVI, 0.09);
+blue = im2double(blue);
+green = im2double(green);
+
+out = cat(3, red, green, blue);
+imshow(out);
+title("Imagem colorida");
+print -djpg geradas/segmentacao16.jpg
+
+ndvi = (nir - red) ./ (nir + red);
+imshow(ndvi);
+title("NDVI");
+print -djpg geradas/segmentacao17.jpg
+
+vegetacao = ndvi;
+vegetacao(find(vegetacao<=0.5)) = 0;
+vegetacao(find(vegetacao>0.5)) = 1;
+imshow(vegetacao);
+title("NDVI Limiarizada - Vegetacao");
+print -djpg geradas/segmentacao18.jpg
+
+sem_vegetacao = 1-vegetacao;
+imshow(sem_vegetacao);
+title("NDVI Limiarizada - Areas sem Vegetacao");
+print -djpg geradas/segmentacao19.jpg
+
 
 # Separação de imagens
 digital = imread("imagens/digital.png");
 imshow(digital);
 digital = im2double(digital);
 imhist(digital);
-d = im2double(digital < 0.8);
 d = im2double(digital > 0.3);
 imshow(d);
-print -djpg geradas/segmentacao12.jpg
+title("Digital - Separada")
+print -djpg geradas/segmentacao20.jpg
