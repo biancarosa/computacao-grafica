60f97fb99d19fe203ac5a4bd8f5b893cb3379ba3
bianca rosa <me@biancarosa.com.br>
2018-11-01 10:20:27 -0300

    Update rotacao.m

--- octave/rotacao.m
+++ octave/rotacao.m
@@ -1,30 +1,34 @@
-P0 = [1 1 1]';
-P1 = [2 1 1]';
-P2 = [2 2 2]';
-P3 = [1 2 1]';
-line([P0(1) P1(1)], [P0(2) P1(2)]);
-line([P1(1) P2(1)], [P1(2) P2(2)]);
-line([P2(1) P3(1)], [P2(2) P3(2)]);
-line([P3(1) P0(1)], [P3(2) P0(2)]);
-# T = [1 0 0; 0 1 0; tx ty 1];
-T = [1 0 0; 0 1 0; -1.5 -1.5 1];
-# centro do objetivo = min() + ((min() - max() / 2)
-# tx - negativo das coordenadas do centro
-# ty - negativo das coordenadas do centro
-alpha = pi / 4;
-R = [cos(alpha) -sin(alpha) 0; sin(alpha) cos(alpha) 0; 0 0 1];
-Ti = inv(T);
-Tf = Ti * R * T;
-P01 = Tf * P0;
-P11 = Tf * P1;
-P21 = Tf * P2;
-P31 = Tf * P3;
-line([P01(1) P11(1)], [P01(2) P11(2)]);
-line([P11(1) P21(1)], [P11(2) P21(2)]);
-line([P21(1) P31(1)], [P21(2) P31(2)]);
-line([P31(1) P01(1)], [P31(2) P01(2)]);
+function rotacao(MP)
+  P0 = MP(1,:)';
+  P1 = MP(2,:)';
+  P2 = MP(3,:)';
+  P3 = MP(4,:)';
+  # T = [1 0 0; 0 1 0; tx ty 1];
+  # centro do objeto = min() + ((min() - max() / 2)
+  # tx - negativo das coordenadas do centro
+  # ty - negativo das coordenadas do centro
+  T = [1 0 0; 0 1 0; tx ty 1];
+  alpha = pi / 4;
+  R = [cos(alpha) -sin(alpha) 0; sin(alpha) cos(alpha) 0; 0 0 1];
+  Ti = inv(T);
+  Tf = Ti * R * T;
+  P01 = Tf * P0;
+  P11 = Tf * P1;
+  P21 = Tf * P2;
+  P31 = Tf * P3;
+  figure;
+  axis([-4, 4, -4, 4], "square");
+  line([P01(1) P11(1)], [P01(2) P11(2)], "color","b");
+  line([P11(1) P21(1)], [P11(2) P21(2)], "color","b");
+  line([P21(1) P31(1)], [P21(2) P31(2)], "color","b");
+  line([P31(1) P01(1)], [P31(2) P01(2)], "color","b");
+endfunction
 
-# primeiro levar pra origem
-# depois esticar
-# depois rotacionar
-# depois levar de volta p origem
+# M = zeros(size(P,2)),
+# IF H(1) != 0, DELETE(H), END
+# H(1) = LINE( ),
+# H2 = 
+# DRAW NOW,
+# PAUSE (01)
+
+# R = [cos alpha -sen alpha 0; cos alpha 0; 0 0 1]
